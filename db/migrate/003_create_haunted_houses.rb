28f74a2c3a3e92410af74cf238dcd4629c57c89b
Daniel Sun <danielsun12345@gmail.com>
2020-03-31 21:22:11 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,11 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  
+  def change 
+    create_table :huanted_houses 
+    
+    
+  end 
+  
+  
+  
+end 
